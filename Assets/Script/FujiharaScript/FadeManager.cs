a85fd8d0b94b3514efb780e236f1b4dd8a860514
keishiF <kf.syukatsu@gmail.com>
2025-10-04 17:55:15 +0900

    up

---
+++ Assets/Script/FujiharaScript/FadeManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour
+{
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1.0f;
+
+    private void Awake()
+    {
+        // 最初はフェードイン
+        fadeImage.gameObject.SetActive(true);
+        fadeImage.color = Color.black;
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOutAndLoadScene(sceneName));
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = 1 - Mathf.Clamp01(t / fadeDuration);
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        fadeImage.gameObject.SetActive(false); // フェード完了後に非表示
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        fadeImage.gameObject.SetActive(true);
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Clamp01(t / fadeDuration);
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
