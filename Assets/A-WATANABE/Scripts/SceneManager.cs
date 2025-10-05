73d1701efeabfbf9d1f558ea2272ecb4cdc71bbc
Namichandesu <watanabe.7310@icloud.com>
2025-10-05 12:11:03 +0900

    test

--- Assets/A-WATANABE/Scripts/SceneManager.cs
+++ Assets/A-WATANABE/Scripts/SceneManager.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttontest: MonoBehaviour
 {
+    [SerializeField] private Button[] buttons;
+    [SerializeField] int selectedIndex;
+    [SerializeField] private Fademanager fadeManager;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per fram
         public void OnPushedButton()
     {
-        SceneManager.LoadScene("GameScene");
+        SeManager seManager = SeManager.Instance;
+        seManager.SettingPlaySE();
+        fadeManager.FadeToScene("GameScene");
     }
 }
 
